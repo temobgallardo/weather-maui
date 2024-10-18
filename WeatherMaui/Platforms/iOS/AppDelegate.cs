6578f64f58ca4e6b936f2024678f0e2465a7adda
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-18 12:12:18 -0600

    first commit

---
+++ WeatherMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WeatherMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
