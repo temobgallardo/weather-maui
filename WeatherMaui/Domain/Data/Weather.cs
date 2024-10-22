99626efec723a4a27bb6bd15a09e5688d4dff9e0
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-22 15:11:33 -0600

    prepared Weather domain model

--- WeatherMaui/Domain/Data/Weather.cs
+++ WeatherMaui/Domain/Data/Weather.cs
@@ -9,39 +9,9 @@ namespace WeatherMaui.Data.Dto;
 public partial class WeatherResponseDto
 {
   public string CityName { get; set; }
-
-  public List<WeatherDto> Weather { get; set; }
-
-  [JsonProperty("base")]
-  public string Base { get; set; }
-
-  [JsonProperty("main")]
-  public MainDto Main { get; set; }
-
-  [JsonProperty("visibility")]
   public long Visibility { get; set; }
-
-  [JsonProperty("wind")]
-  public Wind Wind { get; set; }
-
-  [JsonProperty("clouds")]
+  public WindDto Wind { get; set; }
   public CloudsDto Clouds { get; set; }
-
-  [JsonProperty("dt")]
-  public long Dt { get; set; }
-
-  [JsonProperty("sys")]
-  public SysDto Sys { get; set; }
-
-  [JsonProperty("timezone")]
   public long Timezone { get; set; }
-
-  [JsonProperty("id")]
-  public long Id { get; set; }
-
-  [JsonProperty("name")]
   public string Name { get; set; }
-
-  [JsonProperty("cod")]
-  public long Cod { get; set; }
 }
