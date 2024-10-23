8e5f3c285530478324e91e679150c65fc6d2ac95
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-23 09:39:27 -0600

    protected for possible null properties

--- WeatherMaui/Data/Transformers/WeatherResponseDtoToWeather.cs
+++ WeatherMaui/Data/Transformers/WeatherResponseDtoToWeather.cs
@@ -8,13 +8,15 @@ public class WeatherResponseDtoTransformer
   public static Weather Transform(WeatherResponseDto from) => new()
   {
     City = from.Name,
-    Country = from.Sys.Country,
-    FeelsLike = from.Main.FeelsLike,
-    Humidity = from.Main.Humidity,
-    Pressure = from.Main.Pressure,
-    Temp = from.Main.Temp,
-    TempMax = from.Main.TempMax,
-    TempMin = from.Main.TempMin,
-    // TODO: Fill rest of properties
+    Country = from.Sys?.Country,
+    FeelsLike = (double)from.Main?.FeelsLike,
+    Humidity = (long)from.Main?.Humidity,
+    Name = from.Name,
+    Pressure = (long)from.Main?.Pressure,
+    Temp = (double)from.Main?.Temp,
+    TempMax = (double)from.Main?.TempMax,
+    TempMin = (double)from.Main?.TempMin,
+    Timezone = from.Timezone,
+    Visibility = from.Visibility,
   };
 }
